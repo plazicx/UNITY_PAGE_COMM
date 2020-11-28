02eb2eb9ddec8da436d4ce5b3a8b09934a1d5fdc
Predrag Lazic <plazicx@gmail.com>
2020-11-28 02:23:12 -0600

    this works

---
+++ Assets/Scripts/UI/UIFromUnity.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using TMPro;
+
+/// <summary>
+/// UI Panel for sending message from Unity to a page
+/// </summary>
+public class UIFromUnity : MonoBehaviour
+{
+    // Reference to the input field
+    [SerializeField]
+    private TMP_InputField inputMessage;
+
+    // Reference to the bridge
+    [SerializeField]
+    private BridgeScript bridge;
+
+    /// <summary>
+    /// Method for sending entered message to the bridge
+    /// </summary>
+    public void SendMessageToPage()
+    {
+        // Get a message
+        var message = inputMessage.text;
+        // Clear field
+        inputMessage.text = "";
+        message = "hello from Unity!";
+        // Send a message to the brigde
+        bridge.SendMessageToPage(message);
+    }
+    public void SendMessageToPage2()
+    {
+        // Get a message
+        var message = inputMessage.text;
+        // Clear field
+        inputMessage.text = "";
+        message = "hello from Unity again!";
+        // Send a message to the brigde
+        bridge.SendMessageToPage(message);
+    }
+}
